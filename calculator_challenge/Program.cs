21c6417f15ae7812df980f9eab6ab53e0be820f6
Usman Jamil <usman.jamil@live.co.uk>
2015-11-26 16:58:25 +0000

    added multiplication method

--- calculator_challenge/Program.cs
+++ calculator_challenge/Program.cs
@@ -5,8 +5,19 @@ namespace calculator_challenge
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+		{	
+			Start:
+			int num01;
+			int num02;
+			Console.Write ("Enter a number to be multiplied: ");
+
+			num01 = Convert.ToInt32 (Console.ReadLine ());
+			Console.Write ("Enter the number you want it to be multiplied with: ");
+			num02 = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine (num01 + " multiplied by "+ num02 + " is " + (num01 * num02));
+			Console.ReadKey ();
+			Console.WriteLine ();
+			goto Start;
 		}
 	}
 }
